2e30997e5a6b59258a1301a750222ce3d41c4078
Helge Standal <helge.standal@gmail.com>
2019-12-20 12:10:06 +0100

    First commit

---
+++ do_not_touch.js
@@ -0,0 +1,15 @@
+// Place the ornaments on the tree
+const pos = [[600,350],[900,460],[700,700],[600,900]];
+
+document.querySelectorAll(".ornament").forEach((item, index) => {
+    item.style.left = pos[index][0] + "px";
+    item.style.top  = pos[index][1] + "px";
+});
+
+// Add active class when clicked to all the ornaments
+document.querySelectorAll(".ornament").forEach((item, index) => {
+    item.addEventListener("click", e => {
+        e.currentTarget.classList.toggle("active");
+        e.stopPropagation();
+    }, false);
+});
